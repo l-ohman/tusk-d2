c9a3de069051a312809fd99cbf20a6e38559fa62
l-ohman <jakelohman7@gmail.com>
2023-03-02 10:28:53 -0600

    Search textbox above hero list

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setSelectedHero } from "../../store";
 
@@ -8,24 +8,34 @@ function HeroList() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (selectedHero === {}) console.log("HERE");
+    if (selectedHero === {}) console.log("Selected hero is an empty object");
   }, [selectedHero])
 
+  const [heroSearch, setHeroSearch] = useState("");
+  const handleSearch = (value) => {
+    setHeroSearch(value);
+  }
+
   return (
-    <div className="leftContainer heroIconsContainer">
-      {Object.keys(allHeroes).map((heroId) => {
-        const hero = allHeroes[heroId];
-        return (
-          <img
-            key={heroId}
-            src={`assets/heroIcons/${hero.name.replaceAll(" ", "_")}_icon.webp`}
-            alt={`${hero.name} icon`}
-            onClick={hero.selectable ? () => dispatch(setSelectedHero(hero)) : () => alert("That hero is already selected/banned")}
-            className={`heroIconDraft ${!hero.selectable ? "unselectable" : ""}`}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="leftContainer">
+        <input type="text" placeholder="Search heroname..." id="heroSearch" onChange={(e) => handleSearch(e.target.value)}/>
+        <div className="heroIconsContainer">
+          {Object.keys(allHeroes).map((heroId) => {
+            const hero = allHeroes[heroId];
+            return (
+              <img
+                key={heroId}
+                src={`assets/heroIcons/${hero.name.replaceAll(" ", "_")}_icon.webp`}
+                alt={`${hero.name} icon`}
+                onClick={hero.selectable ? () => dispatch(setSelectedHero(hero)) : () => alert("That hero is already selected/banned")}
+                className={`heroIconDraft${!hero.selectable ? " unselectable" : heroSearch.length <= 1 ? "" : hero.name.toLowerCase().includes(heroSearch.toLowerCase())? " matchSearch" : " doesNotMatchSearch"}`}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 

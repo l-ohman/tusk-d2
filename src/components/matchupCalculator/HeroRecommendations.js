dd32898470de87f0750bd75e53da4a09801d1a98
Jake Lohman <jakelohman7@gmail.com>
2023-05-17 10:13:13 -0500

    Automatically updating suggestions when heroes are picked/banned

--- src/components/matchupCalculator/HeroRecommendations.js
+++ src/components/matchupCalculator/HeroRecommendations.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Matchups from "./Matchups";
 
 export default function HeroRecommendations() {
+  const teams = useSelector((state) => state.matchupCalculator.teams);
   const [update, setUpdate] = useState(false);
+
+  useEffect(() => {
+    setUpdate(true);
+  }, [teams]);
 
   useEffect(() => {
     if (update === true) {
@@ -10,19 +16,10 @@ export default function HeroRecommendations() {
     }
   }, [update]);
 
-  const showBestPicks = () => {
-    setUpdate(true);
-  };
-
   return (
-    <>
-      <div className="tmpCenter">
-        <button onClick={() => showBestPicks()}>Update recommendations</button>
-      </div>
-      <div className="matchupContainer">
-        <Matchups team="Radiant" update={update} />
-        <Matchups team="Dire" update={update} />
-      </div>
-    </>
+    <div className="matchupContainer">
+      <Matchups team="Radiant" update={update} />
+      <Matchups team="Dire" update={update} />
+    </div>
   );
 }

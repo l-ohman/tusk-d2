01346be280aef0439e31f632602d8fddc184cab1
Jake Lohman <jakelohman7@gmail.com>
2023-05-18 15:54:20 -0500

    Updated footer; fixed issue with hovering unselectable heroes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from "react";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div id="footer">
-      <p>
-        work in progress —{" "}
-        <a
-          href="https://github.com/l-ohman/tusk-d2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-      </p>
+      <p>This project is a work in progress</p>
+      <a
+        href="https://github.com/l-ohman/tusk-d2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p>View on Github</p>
+        <FaGithubSquare size={24} />
+      </a>
     </div>
   );
 }

8b94224a3e2293ada9ea6bff8cfe885b98063e7e
Jake Lohman <jakelohman7@gmail.com>
2023-07-27 08:58:08 -0500

    DB to JSON conversion

---
+++ server/dump.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = path.resolve(__dirname, "../tmp/output.sql");
+const output = path.resolve(__dirname, "data.json");
+
+fs.readFile(input, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading the SQL file:", err);
+    return;
+  }
+
+  const lines = data.split("\n").filter((line) => line.trim() !== "");
+  const heroList = lines.filter((line) => {
+    const check = line.slice(0, 3).trimEnd();
+    if (!check) return false;
+    const isHeroId = !isNaN(check);
+    return isHeroId;
+  });
+
+  const heroes = {};
+  for (let i = 0; i < heroList.length; i++) {
+    const heroData = heroList[i].split(/[\s\t]/);
+
+    const heroId = heroData[0];
+    const baseWinrate = heroData[1];
+    const with_json = JSON.parse(heroData[2]);
+    const against_json = JSON.parse(heroData[3]);
+
+    heroes[heroId] = {
+      baseWinrate: Number(baseWinrate),
+      with: with_json,
+      against: against_json,
+    };
+  }
+
+  fs.writeFile(output, JSON.stringify(heroes), (err) => {
+    if (err) {
+      console.error("Error writing the JSON file:", err);
+      return;
+    }
+    console.log("Conversion to JSON successful!");
+  });
+});

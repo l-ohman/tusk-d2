e83e5d43678bdc3cd535f0fb6532b1509764b709
Jake Lohman <jakelohman7@gmail.com>
2023-10-12 17:21:06 -0500

    Displaying all heroes with team logos on proper radiant/dire sides

--- src/components/ti12/MatchList.js
+++ src/components/ti12/MatchList.js
@@ -8,7 +8,7 @@ export default function MatchList() {
 
   return (
     <div id="match-list-wrapper">
-      <h2>Matches</h2>
+      {/* <h2>Matches</h2> */}
       {/* todo: searching/filtering/etc */}
       <div id="match-list-container">
         {allMatches?.map((match, idx) => (
--- src/components/ti12/SingleMatch.js
+++ src/components/ti12/SingleMatch.js
@@ -4,16 +4,45 @@ import { heroNameById, heroIconById } from "../../lib";
 export default function SingleMatch({ match }) {
   return (
     <div className="single-match">
-      <Roster isRadiant={true} pickBans={match.pickBans} />
-      <Roster isRadiant={false} pickBans={match.pickBans} />
+      <div className="team-draft radiant-draft">
+        <Team team={match.radiantTeam} />
+        <div className="pickBan-container">
+          <Roster isRadiant={true} pickBans={match.pickBans} />
+          <Roster isRadiant={true} pickBans={match.pickBans} isPick={false} />
+        </div>
+      </div>
+      <MatchInfo />
+      <div className="team-draft dire-draft">
+        <Team team={match.direTeam} />
+        <div className="pickBan-container">
+          <Roster isRadiant={false} pickBans={match.pickBans} />
+          <Roster isRadiant={false} pickBans={match.pickBans} isPick={false} />
+        </div>
+      </div>
     </div>
   );
 }
 
-function Roster({ isRadiant, pickBans }) {
-  pickBans = pickBans.filter(
-    (itm) => itm.isPick && itm.isRadiant === isRadiant
+function Team({ team }) {
+  return (
+    <div className="single-match-team">
+      {/* <p>{team.name}</p> */}
+      <img src={team.logo} alt={team.name} />
+    </div>
   );
+}
+
+function Roster({ pickBans, isRadiant, isPick = true }) {
+  pickBans = pickBans.filter(
+    (itm) => itm.isPick === isPick && itm.isRadiant === isRadiant
+  );
+  const classNameByFilter = () => {
+    if (isPick) {
+      return isRadiant ? "hero-pick radiant-pick" : "hero-pick dire-pick";
+    } else {
+      return "hero-ban";
+    }
+  };
   return (
     <div className="matchlist-roster">
       {pickBans?.map((hero, i) => {
@@ -22,13 +51,15 @@ function Roster({ isRadiant, pickBans }) {
             <img
               src={heroIconById(hero.heroId)}
               alt={heroNameById(hero.heroId)}
-              className={
-                isRadiant ? "hero-pick radiant-pick" : "hero-pick dire-pick"
-              }
+              className={classNameByFilter()}
             />
           </div>
         );
       })}
     </div>
   );
+}
+
+function MatchInfo() {
+  return <div>Match info</div>;
 }
